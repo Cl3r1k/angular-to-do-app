57aa7bd18c9ee783063da25d37b1724bd8798a2b
Cl3r1k <Cl3r1k2@gmail.com>
2017-09-14 11:06:40 +0600

    Added info about build fot gh-pages

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 // C:\Windows\System32\cmd.exe /k "C:\Program Files\nodejs\nodevars.bat" // To start from VSC terminal node env
 
 // To push changes on githubpages
+// ng build --prod --base-href "https://Cl3r1k.github.io/angular-to-do-app/"
 // angular-cli-ghpages --repo=https://github.com/Cl3r1k/angular-to-do-app.git --name=Cl3r1k --email=cl3r1k2@gmail.com --no-silent
 
 import { Component } from '@angular/core';
