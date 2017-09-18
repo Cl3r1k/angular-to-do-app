26f9343bfa9f582285edf541b339d5013a77c49a
Cl3r1k <Cl3r1k2@gmail.com>
2017-09-18 09:47:32 +0600

    Updated tests, minor changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,6 +4,9 @@
 // ng build --prod --base-href "https://Cl3r1k.github.io/angular-to-do-app/"
 // angular-cli-ghpages --repo=https://github.com/Cl3r1k/angular-to-do-app.git --name=Cl3r1k --email=cl3r1k2@gmail.com --no-silent
 
+// TODO:
+// - При редактировании использовать след. анимацию - затемнять фон, и приближать текущую задачу
+
 import { Component } from '@angular/core';
 import { TodoAppComponent } from './todo-app/todo-app.component';
 
