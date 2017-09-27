a87f981e3183543fa5e50ea942aa7866c9175a1e
Cl3r1k <Cl3r1k2@gmail.com>
2017-09-27 12:40:24 +0600

    Updated code and test to part-3 (HTTP Requests)

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -7,8 +7,8 @@ describe('angular-to-do-app App', () => {
     page = new AppPage();
   });
 
-  it('should display welcome message', () => {
+  it('should display Todo message', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to app!');
+    expect(page.getParagraphText()).toEqual('Todo list(v_0.0.3)');
   });
 });
--- src/app/todo.service.spec.ts
+++ src/app/todo.service.spec.ts
@@ -1,4 +1,6 @@
 import { TestBed, async, inject } from '@angular/core/testing';
+import { ApiService } from './api.service';
+import { ApiMockService } from './api-mock.service';
 
 import { ToDo } from './to-do';
 import { TodoService } from './todo.service';
@@ -6,127 +8,16 @@ import { TodoService } from './todo.service';
 describe('TodoService', () => {
     beforeEach(() => {
         TestBed.configureTestingModule({
-            providers: [TodoService]
+            providers: [TodoService,
+                {
+                    provide: ApiService,
+                    useClass: ApiMockService
+                }
+            ]
         });
     });
 
     it('Should be created', inject([TodoService], (service: TodoService) => {
         expect(service).toBeTruthy();
     }));
-
-    describe('#getAllTodos', () => {
-        it('Should return an empty array by default', inject([TodoService], (service: TodoService) => {
-            // Arrange
-
-            // Act
-
-            // Assert
-            expect(service.getAllTodos()).toEqual([]);
-        }));
-
-        it('Should return all todos', inject([TodoService], (service: TodoService) => {
-            // Arrange
-            const todo1 = new ToDo({ title: 'Hello 1', complete: false });
-            const todo2 = new ToDo({ title: 'Hello 2', complete: true });
-
-            // Act
-            service.addTodo(todo1);
-            service.addTodo(todo2);
-
-            // Assert
-            expect(service.getAllTodos()).toEqual([todo1, todo2]);
-        }));
-    });
-
-    describe('#save(todo)', () => {
-        it('Should automatically assign an incrementing id', inject([TodoService], (service: TodoService) => {
-            // Arrange
-            const todo1 = new ToDo({ title: 'Hello 1', complete: false });
-            const todo2 = new ToDo({ title: 'Hello 2', complete: true });
-
-            // Act
-            service.addTodo(todo1);
-            service.addTodo(todo2);
-
-            // Assert
-            expect(service.getTodoById(1)).toEqual(todo1);
-            expect(service.getTodoById(2)).toEqual(todo2);
-        }));
-    });
-
-    describe('#deleteTodoById(id)', () => {
-        it('Should remove todo with the corresponding ID', inject([TodoService], (service: TodoService) => {
-            // Arrange
-            const todo1 = new ToDo({ title: 'Hello 1', complete: false });
-            const todo2 = new ToDo({ title: 'Hello 2', complete: true });
-
-            // Act
-            service.addTodo(todo1);
-            service.addTodo(todo2);
-
-            // Assert
-            expect(service.getAllTodos()).toEqual([todo1, todo2]);
-            service.deleteTodoById(1);
-            expect(service.getAllTodos()).toEqual([todo2]);
-            service.deleteTodoById(2);
-            expect(service.getAllTodos()).toEqual([]);
-        }));
-
-        it('Should not remove anything if todo with corresponding id is not found', inject([TodoService], (service: TodoService) => {
-            // Arrange
-            const todo1 = new ToDo({ title: 'Hello 1', complete: false });
-            const todo2 = new ToDo({ title: 'Hello 2', complete: true });
-
-            // Act
-            service.addTodo(todo1);
-            service.addTodo(todo2);
-
-            // Assert
-            expect(service.getAllTodos()).toEqual([todo1, todo2]);
-            service.deleteTodoById(3);
-            expect(service.getAllTodos()).toEqual([todo1, todo2]);
-        }));
-    });
-
-    describe('#updateTodoById(id)', () => {
-        it('Should return todo with the corresponding id and updated data', inject([TodoService], (service: TodoService) => {
-            // Arrange
-            const todo1 = new ToDo({ title: 'Hello 1', complete: false });
-
-            // Act
-            service.addTodo(todo1);
-            const updatedTodo = service.updateTodoById(1, { title: 'new title' });
-
-            // Assert
-            expect(updatedTodo.title).toEqual('new title');
-        }));
-
-        it('Should return null, if todo is not found', inject([TodoService], (service: TodoService) => {
-            // Arrange
-            const todo = new ToDo({ title: 'Hello 1', complete: false });
-
-            // Act
-            service.addTodo(todo);
-            const updatedTodo = service.updateTodoById(2, { title: 'new title' });
-
-            // Assert
-            expect(updatedTodo).toEqual(null);
-        }));
-    });
-
-    describe('#toggleTodoComplete(id)', () => {
-        it('Should return updated todo with inverse complete status', inject([TodoService], (service: TodoService) => {
-            // Arrange
-            const todo = new ToDo({ title: 'Hello 1', complete: false });
-
-            // Act
-            service.addTodo(todo);
-            let updatedTodo = service.toggleTodoComplete(todo);
-
-            // Assert
-            expect(updatedTodo.complete).toEqual(true);
-            updatedTodo = service.toggleTodoComplete(todo);
-            expect(updatedTodo.complete).toEqual(false);
-        }));
-    });
 });
