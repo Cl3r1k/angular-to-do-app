a5aa2e6cb5bfb3ff516fa5f3b2cac9083d4f464f
Cl3r1k <Cl3r1k2@gmail.com>
2017-10-07 08:48:22 +0600

    Updated unit tests

--- src/app/services/api.service.spec.ts
+++ src/app/services/api.service.spec.ts
@@ -1,28 +1,49 @@
-import { MockBackend } from '@angular/http/testing';
-import { BaseRequestOptions, Http } from '@angular/http';
-import { TestBed, inject } from '@angular/core/testing';
+import { environment } from '../../environments/environment';
+import { ToDo } from './../to-do';
+// import { MockBackend } from '@angular/http/testing';
+// import { BaseRequestOptions, Http } from '@angular/http';
+import { TestBed, getTestBed } from '@angular/core/testing';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 
 import { ApiService } from './api.service';
 
 describe('ApiService', () => {
+
+    let injector: TestBed;
+    let service: ApiService;
+    let httpMock: HttpTestingController;
+
+    const API_URL = environment.apiUrl;
+
     beforeEach(() => {
         TestBed.configureTestingModule({
-            providers: [
-                {
-                    provide: Http,
-                    useFactory: (backend, options) => {
-                        return new Http(backend, options);
-                    },
-                    deps: [ MockBackend, BaseRequestOptions ]
-                },
-                MockBackend,
-                BaseRequestOptions,
-                ApiService
-            ]
+            imports: [HttpClientTestingModule],
+            providers: [HttpTestingController, ApiService]
         });
+
+        injector = getTestBed();
+        service = injector.get(ApiService);
+        httpMock = injector.get(HttpTestingController);
     });
 
-    it('should be created', inject([ApiService], (service: ApiService) => {
-        expect(service).toBeTruthy();
-    }));
+    describe(`#getAllTodos`, () => {
+        it(`should return an Observable<ToDo[]>`, () => {
+            // Arrange
+            const dummyTodos = [
+                new ToDo({ id: 1, title: 'todo 1', complete: false}),
+                new ToDo({ id: 2, title: 'todo 2', complete: true}),
+            ];
+
+            // Act
+            service.getAllTodos().subscribe(todos => {
+                expect(todos.length).toBe(2);
+                expect(todos).toEqual(dummyTodos);
+            });
+
+            // Assert
+            const req = httpMock.expectOne(API_URL + '/todos');
+            expect(req.request.method).toBe('GET');
+            req.flush(dummyTodos);
+        });
+    });
 });
