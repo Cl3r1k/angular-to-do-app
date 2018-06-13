5e62c0f43b4a0af7c8fc256043f432f11aa502bc
Cl3r1k <Cl3r1k2@gmail.com>
2018-06-13 09:24:34 +0600

    Restored 'getTagColor()' in 'IndexedDbService' for testing purpose Updates in 'IndexedDbService' Updates in 'ParseTagPipe'

--- src/app/_pipes/parse-tag.pipe.ts
+++ src/app/_pipes/parse-tag.pipe.ts
@@ -20,46 +20,57 @@ export class ParseTagPipe implements PipeTransform {
     constructor(private _tagService: TagService) { }
 
     transform(text: string): Observable<string> {
-        // this.parseTag(text);
-        // return text;
         return this.parseTag(text);
     }
 
-    parseTag(text: string): Observable<string> {
-        // Find/Replace URL's in text
-        if (text.match(this.urlsRegExp)) {
-            text = text.replace(this.urlsRegExp, function replacer($1, $2, $3) {
-                const url = $1;
-                const urlClean = url.replace('' + $3 + '://', '');
+    // parseTag(text: string): Observable<string> {
+    //     // Find/Replace URL's in text
+    //     if (text.match(this.urlsRegExp)) {
+    //         text = text.replace(this.urlsRegExp, function replacer($1, $2, $3) {
+    //             const url = $1;
+    //             const urlClean = url.replace('' + $3 + '://', '');
 
-                return `<a href='` + url + `' target='_blank'>` + urlClean + `</a>`;
-            });
-        }
+    //             return `<a href='` + url + `' target='_blank'>` + urlClean + `</a>`;
+    //         });
+    //     }
+
+    //     // Find/Replace #hashtags in text
+    //     if (text.match(this.hashtagsRegExp)) {
+    //         // text = text.replace(this.hashtagsRegExp, `$1<span class='tag-class'>$2</span>`);    // Old text without color
+
+    //         // const color = '#efefef';
+    //         // text = text.replace(this.hashtagsRegExp, `$1<span class='tag-class' style='background-color: ` + color + `;'>$2</span>`);
+    //         const scope = this;
+
+    //         text = text.replace(this.hashtagsRegExp, function replacer($1, $2, $3) {
+    //             const space = $2;
+    //             const tagName = $3;
+
+    //             let colorInTagService = '';
+    //             colorInTagService = scope._tagService.getTagColorByName(tagName);
+    //             console.log(`%cin ParseTagPipe for %s colorInTagService is:`, scope.consoleTextColorPipe, tagName, colorInTagService);
+    //             console.log(`%cin ParseTagPipe for %s tags is:`, scope.consoleTextColorPipe, tagName, scope._tagService.tags);
+
+    //             return space + `<span class='tag-class' style='background-color: ` + colorInTagService + `;'>` + tagName + `</span>`;
+    //         });
+    //     }
+
+    //     return Observable.of(text);
+    // }
+
+    parseTag(text: string): Observable<string> {
 
         // Find/Replace #hashtags in text
         if (text.match(this.hashtagsRegExp)) {
-            // text = text.replace(this.hashtagsRegExp, `$1<span class='tag-class'>$2</span>`);    // Old text without color
 
-            // const color = '#efefef';
-            // text = text.replace(this.hashtagsRegExp, `$1<span class='tag-class' style='background-color: ` + color + `;'>$2</span>`);
-            const scope = this;
+            // TODO: Stopped here, added method 'getTagColor()' in 'IndexedDbService', now test in the pipe
+            // Currently the problem with 'newTodo' that pipe in view works faster, then todoList updates in service
+            // Consider to use async pipe and or something else to solve the issue
 
-            text = text.replace(this.hashtagsRegExp, function replacer($1, $2, $3) {
-                const space = $2;
-                const tagName = $3;
-
-                let colorInTagService = '';
-                colorInTagService = scope._tagService.getTagColorByName(tagName);
-                console.log(`%cin ParseTagPipe for %s colorInTagService is:`, scope.consoleTextColorPipe, tagName, colorInTagService);
-                console.log(`%cin ParseTagPipe for %s tags is:`, scope.consoleTextColorPipe, tagName, scope._tagService.tags);
-
-                return space + `<span class='tag-class' style='background-color: ` + colorInTagService + `;'>` + tagName + `</span>`;
-            });
+            const colorInTagService = 'red';
+            const tagName = '#someKindOf';
+            text = `3. Todo with <span class='tag-class' style='background-color: ` + colorInTagService + `;'>` + tagName + `</span>`;
         }
-
-        // TODO: Stopped here, fix tags when they deleted in IndexedDb directly
-        // And currently the problem with 'newTodo' that pipe in view works faster, then tadList updates in service
-        // Consider to use async pipe and or something else to solve the issue
 
         return Observable.of(text);
     }
