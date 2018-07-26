bad29043b72e0af58e868dc83fd523e3260c9e58
Cl3r1k <Cl3r1k2@gmail.com>
2018-07-26 09:11:52 +0600

    Updated tests for 'ParseTagPipe'

--- src/app/_pipes/parse-tag.pipe.spec.ts
+++ src/app/_pipes/parse-tag.pipe.spec.ts
@@ -1,9 +1,20 @@
 import { ParseTagPipe } from '@app/_pipes/parse-tag.pipe';
 
-describe('ParseTagPipe', () => {
-    it('create an instance', () => {
+describe(`Pipe: ParseTagPipe`, () => {
+    let pipe: ParseTagPipe;
+
+    beforeEach(() => {
         // TODO: replace param 'null' to proper param, and complete tests
-        const pipe = new ParseTagPipe(null);
+        // look here (https://codecraft.tv/courses/angular/unit-testing/angular-test-bed/)
+        // And use mocked parseTag() (look here https://codecraft.tv/courses/angular/unit-testing/mocks-and-spies/)
+        pipe = new ParseTagPipe(null);
+    });
+
+    it(`create an instance`, () => {
         expect(pipe).toBeTruthy();
+    });
+
+    it(`providing no value returns empty string`, () => {
+        expect(pipe.transform('')).toBe('');
     });
 });
