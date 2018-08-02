d0f3ed3e1a2536d2ec0c2773c814412ab9640fa3
Cl3r1k <Cl3r1k2@gmail.com>
2018-08-02 09:02:40 +0600

    Updates in tests for 'DynamicContentDirective' Minor changes

--- src/app/_directives/dynamic-content.directive.spec.ts
+++ src/app/_directives/dynamic-content.directive.spec.ts
@@ -1,16 +1,42 @@
-import { TestBed } from '@angular/core/testing';
+import { TestBed, ComponentFixture } from '@angular/core/testing';
+import { Component } from '@angular/core';
+import { By } from '@angular/platform-browser';
+import { RouterTestingModule } from '@angular/router/testing';
 
+// Directive
 import { DynamicContentDirective } from '@app/_directives/dynamic-content.directive';
 
+@Component({
+    template: `<div class='tag-class' appDynamicContentDirective>some #hashtag</div>`
+})
+class TestDynamicContentDirectiveComponent {
+    //
+}
+
 describe(`Directive: DynamicContentDirective`, () => {
+
     let directive: DynamicContentDirective;
+    let component: TestDynamicContentDirectiveComponent;
+    let fixture: ComponentFixture<TestDynamicContentDirectiveComponent>;
+    let divEl;
 
     beforeEach(() => {
         TestBed.configureTestingModule({
+            imports: [RouterTestingModule],
+            declarations: [DynamicContentDirective, TestDynamicContentDirectiveComponent],
             providers: []
         });
 
+        fixture = TestBed.createComponent(TestDynamicContentDirectiveComponent);
+        component = fixture.componentInstance;
+
+        divEl = fixture.debugElement.query(By.css('div'));    // Find div.todoTitle element
+
         // TODO: replace param 'null' to proper param, and complete tests
+        // Examples are here: https://codecraft.tv/courses/angular/unit-testing/directives/
+        // and here: https://stackoverflow.com/questions/36432407/how-to-unit-test-a-directive-in-angular-2
+        // Test ElementRef
+        // look here: https://stackoverflow.com/questions/38623065/angular2-inject-elementref-in-unit-test
         directive = new DynamicContentDirective(null, null);
     });
 
