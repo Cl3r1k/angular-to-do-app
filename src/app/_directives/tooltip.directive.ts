8153a4fdecf4052afa7d97dcf0aa8e384e913f01
Cl3r1k <Cl3r1k2@gmail.com>
2018-08-18 09:10:04 +0600

    Updated 'TooltipDirective' Added styles file '_tooltip.scss' Minor changes

--- src/app/_directives/tooltip.directive.ts
+++ src/app/_directives/tooltip.directive.ts
@@ -42,11 +42,52 @@ export class TooltipDirective {
     }
 
     create() {
-        //
+        this.tooltip = this.renderer.createElement('span');
+
+        this.renderer.appendChild(this.tooltip, this.renderer.createText(this.toolTipTitle));
+        this.renderer.appendChild(document.body, this.tooltip);
+
+        this.renderer.addClass(this.tooltip, 'tooltip');
+        this.renderer.addClass(this.tooltip, `tooltip-${this.placement}`);
+
+        // Delay
+        this.renderer.setStyle(this.tooltip, '-webkit-transition', `opacity ${this.delay}ms`);
+        this.renderer.setStyle(this.tooltip, '-moz-transition', `opacity ${this.delay}ms`);
+        this.renderer.setStyle(this.tooltip, '-o-transition', `opacity ${this.delay}ms`);
+        this.renderer.setStyle(this.tooltip, 'transition', `opacity ${this.delay}ms`);
     }
 
     setPosition() {
-        //
+        const hostPos = this.el.nativeElement.getBoundingClientRect();
+
+        const tooltipPos = this.tooltip.getBoundingClientRect();
+
+        const scrollPos = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0;
+
+        let top, left;
+
+        if (this.placement === 'top') {
+            top = hostPos.top - tooltipPos.height - this.offset;
+            left = hostPos.left + (hostPos.width - tooltipPos.width) / 2;
+        }
+
+        if (this.placement === 'bottom') {
+            top = hostPos.bottom + this.offset;
+            left = hostPos.left + (hostPos.width - tooltipPos.width) / 2;
+        }
+
+        if (this.placement === 'left') {
+            top = hostPos.top + (hostPos.height - tooltipPos.height) / 2;
+            left = hostPos.left - tooltipPos.width - this.offset;
+        }
+
+        if (this.placement === 'right') {
+            top = hostPos.top + (hostPos.height - tooltipPos.height) / 2;
+            left = hostPos.right + this.offset;
+        }
+
+        this.renderer.setStyle(this.tooltip, 'top', `${top + scrollPos}px`);
+        this.renderer.setStyle(this.tooltip, 'left', `${left}px`);
     }
 
 }
