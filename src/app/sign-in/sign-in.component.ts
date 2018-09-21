72bdc21d1d6c4a0d45b74404a64addec9af36d85
Cl3r1k <Cl3r1k2@gmail.com>
2018-09-21 09:09:52 +0600

    Updated 'SignInComponent' Updates in 'SignInComponent' template Minor changes

--- src/app/sign-in/sign-in.component.ts
+++ src/app/sign-in/sign-in.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { ApiService } from '@app/_services/api.service';
+import { AuthService } from '@app/_services/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
     selector: 'app-sign-in',
@@ -7,9 +11,52 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SignInComponent implements OnInit {
 
-    constructor() { }
+    public frm: FormGroup;
+
+    public isBusy = false;
+    public hasFailed = false;
+    public showInputErrors = false;
+
+    constructor(
+        private _apiService: ApiService,
+        private _authService: AuthService,
+        private _formBuilder: FormBuilder,
+        private _router: Router
+    ) {
+        this.frm = _formBuilder.group({
+            username: ['', Validators.required],
+            password: ['', Validators.required]
+        });
+    }
 
     ngOnInit() {
+    }
+
+    public doSignIn() {
+
+        // Make sure form values are valid
+        if (this.frm.invalid) {
+            this.showInputErrors = true;
+            return;
+        }
+
+        // Reset status
+        this.isBusy = true;
+        this.hasFailed = false;
+
+        // Grab values from form
+        const username = this.frm.get('username').value;
+        const password = this.frm.get('password').value;
+
+        // Submit request to API
+        this._apiService.signIn(username, password).subscribe((response) => {
+            this._authService.doSignIn(response['token'], response['name']);
+            this._router.navigate(['todos']);
+        },
+            (error) => {
+                this.isBusy = false;
+                this.hasFailed = true;
+            });
     }
 
 }
