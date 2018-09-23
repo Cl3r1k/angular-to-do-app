a84e68a90171110ddddccb703db4365273784723
Cl3r1k <Cl3r1k2@gmail.com>
2018-09-23 09:05:54 +0600

    Updates in 'CanActivateTodosGuard' Minor changes

--- src/app/_guards/can-activate-todos.guard.ts
+++ src/app/_guards/can-activate-todos.guard.ts
@@ -1,14 +1,23 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router } from '@angular/router';
 import { Observable } from 'rxjs';
+import { AuthService } from '@app/_services/auth.service';
 
 @Injectable({
     providedIn: 'root'
 })
 export class CanActivateTodosGuard implements CanActivate {
+
+    constructor(private _authService: AuthService, private _router: Router) { }
+
     canActivate(
-        next: ActivatedRouteSnapshot,
+        route: ActivatedRouteSnapshot,
         state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
-        return true;
+        if (!this._authService.isSignedIn()) {
+            this._router.navigate(['/sign-in']);
+            return false;
+        }
+
+        return false;
     }
 }
