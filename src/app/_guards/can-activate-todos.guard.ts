c5dcacc435716797df07728b6a8bb2578883ee0f
Cl3r1k <Cl3r1k2@gmail.com>
2018-10-05 11:29:29 +0600

    In 'package.json' script 'app' renamed to 'app-old', and script 'app-auth' renamed to 'app' Minor changes

--- src/app/_guards/can-activate-todos.guard.ts
+++ src/app/_guards/can-activate-todos.guard.ts
@@ -11,6 +11,7 @@ export class CanActivateTodosGuard implements CanActivate {
     constructor(private _authService: AuthService, private _router: Router) { }
 
     // TODO: Add protection for child (CanActivateChild) and test with route 'http://localhost:4200/todos/filter/hashtag/%23tagName'
+    // Take a look closer on CanActivateChild, should we use in for each child component?
     canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
         if (!this._authService.isSignedIn()) {
             console.error('Access denied - Redirect to sign-in page');
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -5,5 +5,5 @@ export const environment = {
     apiUrl: 'https://github.com/Cl3r1k/angular-to-do-app/db.json',
 
     // Autoincremented build version variable
-    version: '1.4.4.723'
+    version: '1.4.4.724'
 };
