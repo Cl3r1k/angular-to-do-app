cf7f024b8d4fb148911373214220ea8a50292b94
Cl3r1k <Cl3r1k2@gmail.com>
2018-10-06 09:24:15 +0600

    Minor changes

--- src/app/_guards/can-activate-todos.guard.ts
+++ src/app/_guards/can-activate-todos.guard.ts
@@ -11,7 +11,8 @@ export class CanActivateTodosGuard implements CanActivate {
     constructor(private _authService: AuthService, private _router: Router) { }
 
     // TODO: Add protection for child (CanActivateChild) and test with route 'http://localhost:4200/todos/filter/hashtag/%23tagName'
-    // Take a look closer on CanActivateChild, should we use in for each child component?
+    // Take a look closer on CanActivateChild, should we use it for each child component?
+    // Example here 'https://scotch.io/courses/routing-angular-2-applications/canactivate-and-canactivatechild'
     canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
         if (!this._authService.isSignedIn()) {
             console.error('Access denied - Redirect to sign-in page');
