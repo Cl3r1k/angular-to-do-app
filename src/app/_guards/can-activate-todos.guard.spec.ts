d5b23d3ea9a63cc91c39be4db9d51e42194fad96
Cl3r1k <Cl3r1k2@gmail.com>
2018-10-12 09:06:40 +0600

    ✔ Updated tests for 'CanActivateTodosGuard' Minor changes

--- src/app/_guards/can-activate-todos.guard.spec.ts
+++ src/app/_guards/can-activate-todos.guard.spec.ts
@@ -1,10 +1,21 @@
 import { TestBed, async, inject } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 
 import { CanActivateTodosGuard } from '@app/_guards/can-activate-todos.guard';
 
-describe('CanActivateTodosGuard', () => {
+class MockRouter {
+    navigate(path) { }
+}
+
+describe('Guard: CanActivateTodosGuard', () => {
+
+    let todosGuard: CanActivateTodosGuard;
+    let authService;
+    let router;
+
     beforeEach(() => {
         TestBed.configureTestingModule({
+            imports: [RouterTestingModule],
             providers: [CanActivateTodosGuard]
         });
     });
@@ -12,4 +23,30 @@ describe('CanActivateTodosGuard', () => {
     it('should ...', inject([CanActivateTodosGuard], (guard: CanActivateTodosGuard) => {
         expect(guard).toBeTruthy();
     }));
+
+    it(`should return true for a logged in user`, () => {
+        // Arrange
+        authService = { isSignedIn: () => true };
+        router = new MockRouter();
+        todosGuard = new CanActivateTodosGuard(authService, router);
+
+        // Act
+
+        // Assert
+        expect(todosGuard.canActivate(null, null)).toEqual(true);
+    });
+
+    it(`should navigate to 'sign-in' page for a logged out user`, () => {
+        // Arrange
+        authService = { isSignedIn: () => false };
+        router = new MockRouter();
+        todosGuard = new CanActivateTodosGuard(authService, router);
+        spyOn(router, 'navigate');
+
+        // Act
+
+        // Assert
+        expect(todosGuard.canActivate(null, null)).toEqual(false);
+        expect(router.navigate).toHaveBeenCalledWith(['/sign-in']);
+    });
 });
