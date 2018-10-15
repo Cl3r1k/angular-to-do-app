8003872e2a2a1347e0c7087a59a98dfaaf451e79
Cl3r1k <Cl3r1k2@gmail.com>
2018-10-15 09:32:02 +0600

    Minor changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { PageNotFoundComponent } from '@app/page-not-found/page-not-found.component';
-import { TodosComponent } from '@app/todos/todos.component';
+
 import { TodosResolver } from '@app/_resolvers/todos.resolver';
 import { TodosActiveResolver } from '@app/_resolvers/todos-active.resolver';
 import { TodosCompletedResolver } from '@app/_resolvers/todos-completed.resolver';
 import { TodosFilterHashtagResolver } from '@app/_resolvers/todos-filter-hashtag.resolver';
+
+// Components
+import { PageNotFoundComponent } from '@app/page-not-found/page-not-found.component';
+import { TodosComponent } from '@app/todos/todos.component';
 import { SignInComponent } from '@app/sign-in/sign-in.component';
 
 // Guards
