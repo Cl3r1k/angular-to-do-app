d28266804f387ec7b2433e13024cc82f4d7a571a
Cl3r1k <Cl3r1k2@gmail.com>
2018-10-28 08:56:06 +0600

    Updates in tests for 'SignInComponent'

--- src/app/sign-in/sign-in.component.spec.ts
+++ src/app/sign-in/sign-in.component.spec.ts
@@ -1,4 +1,5 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
 import { ReactiveFormsModule } from '@angular/forms';
 import { RouterTestingModule } from '@angular/router/testing';
 
@@ -12,6 +13,8 @@ import { SignInComponent } from './sign-in.component';
 describe('SignInComponent', () => {
     let component: SignInComponent;
     let fixture: ComponentFixture<SignInComponent>;
+    let inputUsernameEl;
+    let inputPasswordEl;
 
     beforeEach(async(() => {
         TestBed.configureTestingModule({
@@ -30,6 +33,17 @@ describe('SignInComponent', () => {
     beforeEach(() => {
         fixture = TestBed.createComponent(SignInComponent);
         component = fixture.componentInstance;
+        component.ngOnInit();
+        fixture.detectChanges();
+
+        // inputUsernameEl = fixture.debugElement.query(By.css('input[formControlName=username]'));        // Find input username element
+        // inputPasswordEl = fixture.debugElement.query(By.css('input[formControlName=password]'));        // Find input password element
+
+        // inputUsernameEl.value = 'demoName';
+        // inputPasswordEl.value = 'demoPass';
+        // console.log('%cinputUsernameEl:', 'color: pink;', inputUsernameEl);
+        // console.log('%cinputPasswordEl:', 'color: pink;', inputPasswordEl);
+
         fixture.detectChanges();
     });
 
@@ -52,6 +66,59 @@ describe('SignInComponent', () => {
         expect(component.hasFailed).toEqual(false);
         expect(component.showInputErrors).toEqual(false);
         expect(component.authType).toEqual('');
+    });
+
+    it(`form invalid when empty`, () => {
+        // Arrange
+
+        // Act
+
+        // Assert
+        expect(component.frm.valid).toBeFalsy();
+    });
+
+    it(`'username' field validity`, () => {
+        // Arrange
+
+        // Act
+        const username = component.frm.controls['username'];
+
+        // Assert
+        expect(username.valid).toBeFalsy();
+    });
+
+    it(`'username' field 'required' validity`, () => {
+        // Arrange
+        let errors = {};
+
+        // Act
+        const username = component.frm.controls['username'];
+        errors = username.errors || {};
+
+        // Assert
+        expect(errors['required']).toBeTruthy();
+    });
+
+    it(`'password' field validity`, () => {
+        // Arrange
+
+        // Act
+        const password = component.frm.controls['password'];
+
+        // Assert
+        expect(password.valid).toBeFalsy();
+    });
+
+    it(`'password' field 'required' validity`, () => {
+        // Arrange
+        let errors = {};
+
+        // Act
+        const password = component.frm.controls['password'];
+        errors = password.errors || {};
+
+        // Assert
+        expect(errors['required']).toBeTruthy();
     });
 
     describe(`#doSignIn()`, () => {
@@ -77,6 +144,86 @@ describe('SignInComponent', () => {
             // Assert
             // expect(sessionStorageService.accessToken).toEqual(null);
             // expect(sessionStorageService.name).toEqual(null);
+        });
+    });
+
+    describe(`#view tests:`, () => {
+
+        describe(`input.username:`, () => {
+            it(`'blur' should change 'username' state (async)`, async(() => {
+                // Arrange
+
+                // Act
+                // spyOn(component, 'setCompleteHover');
+
+                // Set checkbox.toggle 'mouseenter' hover state
+                // let usernameField = component.frm.get('username').hasError('required');
+                // console.log('%cusernameField:', 'color: pink;', usernameField);
+                // inputUsernameEl.triggerEventHandler('click', null);
+                // console.log('inputUsernameEl:', inputUsernameEl.value);
+                // fixture.detectChanges();
+                // console.log('%cinputUsernameEl:', 'color: pink;', inputUsernameEl);
+
+                // inputUsernameEl.value = '123';
+                // console.log('inputUsernameEl:', inputUsernameEl.value);
+                // usernameField = component.frm.get('username').hasError('required');
+                // console.log('%cusernameField:', 'color: pink;', usernameField);
+                console.log('frm.value: ', component.frm.value);
+                console.log('frm.invalid: ', component.frm.invalid);
+
+                // Assert
+                // fixture.whenStable().then(() => {
+                //     expect(component.setCompleteHover).toHaveBeenCalled();
+                // });
+            }));
+            // it(`'mouseenter' on 'checkbox.toggle' should call method 'setCompleteHover()' (async)`, async(() => {
+            //     // Arrange
+
+            //     // Act
+            //     spyOn(component, 'setCompleteHover');
+
+            //     // Set checkbox.toggle 'mouseenter' hover state
+            //     toggleEl.triggerEventHandler('mouseenter', null);
+            //     fixture.detectChanges();
+
+            //     // Assert
+            //     fixture.whenStable().then(() => {
+            //         expect(component.setCompleteHover).toHaveBeenCalled();
+            //     });
+            // }));
+
+            // it(`'mouseleave' on 'checkbox.toggle' should call method 'setCompleteHover()' (async)`, async(() => {
+            //     // Arrange
+
+            //     // Act
+            //     spyOn(component, 'setCompleteHover');
+
+            //     // Set checkbox.toggle 'mouseleave' hover state
+            //     toggleEl.triggerEventHandler('mouseleave', null);
+            //     fixture.detectChanges();
+
+            //     // Assert
+            //     fixture.whenStable().then(() => {
+            //         expect(component.setCompleteHover).toHaveBeenCalled();
+            //     });
+            // }));
+
+            // it(`clicking on 'checkbox.toggle' should call method 'toggleTodoComplete()' event (async)`, async () => {
+            //     // Arrange
+
+            //     // Act
+            //     spyOn(component, 'toggleTodoComplete');
+            //     if (toggleEl instanceof HTMLElement) {
+            //         toggleEl.click();
+            //     } else {
+            //         toggleEl.triggerEventHandler('click', { button: 0 });
+            //     }
+
+            //     // Assert
+            //     fixture.whenStable().then(() => {
+            //         expect(component.toggleTodoComplete).toHaveBeenCalled();
+            //     });
+            // });
         });
     });
 });
